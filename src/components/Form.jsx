4c4a88f844af4fd22ac2cc8b25a171a5422787ff
sujalamatya <sujalcr10@gmail.com>
2024-08-07 10:09:21 +0545

    Added Submision page and Edit form through lists

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -60,6 +60,7 @@ const Form = ({ editdata }) => {
 
     setData(combinedData);
     console.log(combinedData);
+    setCurrent((current) => current + 1);
   };
 
   const updateFormData = (data) => {
@@ -68,79 +69,95 @@ const Form = ({ editdata }) => {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="max-w-md mx-auto p-4 bg-slate-700 shadow-xl rounded-lg shadow-black bg-blend-darken"
-    >
-      {current === 1 && (
-        <PersonalForm
-          current={current}
-          setCurrent={setCurrent}
-          defaultValues={formData}
-          updateFormData={updateFormData}
-        />
-      )}
-      {current === 2 && (
-        <section>
-          <legend className="text-white text-center text-2xl font-bold">
-            Parental Information
-          </legend>
-          {fields.map((field, index) => (
-            <div key={field.id}>
-              <label htmlFor="" className="text-gray-300">
-                Parent's Name:
-              </label>
-              <input
-                {...register(`parents.${index}.Name`)}
-                className="w-full px-3 py-2 border border-gray-300 rounded-md"
-                placeholder="Parent's Name"
-              />
-              <p className="text-red-500">
-                {errors.parents?.[index]?.Name &&
-                  errors.parents?.[index]?.Name.message}
-              </p>
-              <label htmlFor="" className="text-white mt-2 mb-2">
-                Phone Number:
-              </label>
-              <input
-                {...register(`parents.${index}.number`)}
-                className="w-full px-3 py-2 border border-gray-300 rounded-md"
-                placeholder="Enter phone number"
-              />
-              <p className="text-red-500">
-                {errors.parents?.[index]?.number &&
-                  errors.parents?.[index]?.number.message}
-              </p>
-              <button
-                type="button"
-                onClick={() => remove(index)}
-                className="w-full bg-red-500 text-white py-2 rounded-md hover:bg-red-600 transition duration-200 mt-4"
-              >
-                Remove
-              </button>
-            </div>
-          ))}
-          <button
-            type="button"
-            onClick={() => append({ Name: "", number: "" })}
-            className="w-full bg-green-500 text-white py-2 rounded-md hover:bg-green-600 transition duration-200 mt-4"
-          >
-            Add Parent
-          </button>
-          <button
-            type="button"
-            onClick={prev}
-            className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition duration-200 mt-4"
-          >
-            Prev
-          </button>
-          <input
-            type="submit"
-            className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition duration-200 mt-4"
+    <>
+      <h1
+        className={`text-center text-5xl p-4 mt-3 mb-4 ${
+          current == 3 ? "hidden" : "block"
+        }`}
+      >
+        Complete the Form Below
+      </h1>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className={`max-w-md mx-auto p-4 bg-slate-700 shadow-xl rounded-lg shadow-black bg-blend-darken ${
+          current == 3 ? "hidden" : "block"
+        }`}
+      >
+        {current === 1 && (
+          <PersonalForm
+            current={current}
+            setCurrent={setCurrent}
+            defaultValues={formData}
+            updateFormData={updateFormData}
           />
-        </section>
+        )}
+        {current === 2 && (
+          <section>
+            <legend className="text-white text-center text-2xl font-bold">
+              Parental Information
+            </legend>
+            {fields.map((field, index) => (
+              <div key={field.id}>
+                <label htmlFor="" className="text-gray-300">
+                  Parent's Name:
+                </label>
+                <input
+                  {...register(`parents.${index}.Name`)}
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md"
+                  placeholder="Parent's Name"
+                />
+                <p className="text-red-500">
+                  {errors.parents?.[index]?.Name &&
+                    errors.parents?.[index]?.Name.message}
+                </p>
+                <label htmlFor="" className="text-white mt-2 mb-2">
+                  Phone Number:
+                </label>
+                <input
+                  {...register(`parents.${index}.number`)}
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md"
+                  placeholder="Enter phone number"
+                />
+                <p className="text-red-500">
+                  {errors.parents?.[index]?.number &&
+                    errors.parents?.[index]?.number.message}
+                </p>
+                <button
+                  type="button"
+                  onClick={() => remove(index)}
+                  className="w-full bg-red-500 text-white py-2 rounded-md hover:bg-red-600 transition duration-200 mt-4"
+                >
+                  Remove
+                </button>
+              </div>
+            ))}
+            <button
+              type="button"
+              onClick={() => append({ Name: "", number: "" })}
+              className="w-full bg-green-500 text-white py-2 rounded-md hover:bg-green-600 transition duration-200 mt-4"
+            >
+              Add Parent
+            </button>
+            <button
+              type="button"
+              onClick={prev}
+              className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition duration-200 mt-4"
+            >
+              Prev
+            </button>
+            <input
+              type="submit"
+              className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition duration-200 mt-4"
+            />
+          </section>
+        )}
+      </form>
+      {current === 3 && (
+        <h1 className="text-5xl p-2 mb-2 mx-auto flex items-center text-center justify-center h-screen overflow-hidden box-border">
+          Thank you for submitting the form
+        </h1>
       )}
-    </form>
+    </>
   );
 };
 
